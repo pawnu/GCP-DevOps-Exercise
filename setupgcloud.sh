45a2456ba3a7c52c26c828c4d80a5cc2e4915e93
DaniellaOLee <daolee94@outlook.com>
2017-07-11 22:49:02 +0100

    Update setupgcloud.sh
    changing the device.conf directory

--- setupgcloud.sh
+++ setupgcloud.sh
@@ -2,8 +2,8 @@
 
 project=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
 
-#Create device.conf file at ~/.puppet/ directory
-cat > ~/.puppet/device.conf << EOF
+#Create device.conf file at ~/.puppet/ directory, or if using sudo
+cat > /etc/puppet/device.conf << EOF
 [my_project]
   type gce
   url [/dev/null]:${project}
