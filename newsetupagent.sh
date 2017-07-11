294bd2276439b1a1fc0267cf284d4b06c044cf72
DaniellaOLee <daolee94@outlook.com>
2017-07-11 16:56:05 +0100

    Create newsetupagent.sh
    append individual provisioning files with master facts

---
+++ newsetupagent.sh
@@ -0,0 +1,18 @@
+master=$(facter fqdn)
+ip=$(facter ipaddress)
+dir=$(pwd)
+
+sudo sed -i "1 a masterfqdn=\\$master" $dir/jenkins.sh
+sudo sed -i "1 a masterip=\\$ip" $dir/jenkins.sh
+
+sudo sed -i "1 a masterfqdn=\\$master" $dir/sonarqube.sh
+sudo sed -i "1 a masterip=\\$ip" $dir/sonarqube.sh
+
+sudo sed -i "1 a masterfqdn=\\$master" $dir/tomcat.sh
+sudo sed -i "1 a masterip=\\$ip" $dir/tomcat.sh
+
+sudo sed -i "1 a masterfqdn=\\$master" $dir/nexus.sh
+sudo sed -i "1 a masterip=\\$ip" $dir/nexus.sh
+
+sudo sed -i "1 a masterfqdn=\\$master" $dir/splunk.sh
+sudo sed -i "1 a masterip=\\$ip" $dir/splunk.sh
